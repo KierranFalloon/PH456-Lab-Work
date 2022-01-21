0f7b752d0aae2ec48243a8716b015e259ac185b5
Kierran Falloon <kgwfalloon@gmail.com>
2022-01-21 21:41:35 +0000

    Fixed file, added timer

--- Lab_1.py
+++ Lab_1.py
@@ -8,8 +8,9 @@
 import numpy as np
 from numpy.random import Generator, PCG64, MT19937
 import matplotlib.pyplot as plt
-import time
+from time import perf_counter
 from scipy.stats import chisquare
+
 
 # --------
 # Using different initial seeds and at least two different pseudo-random
@@ -24,18 +25,26 @@ from scipy.stats import chisquare
 # -------- Using two different seeds on the same generator should result in reproducible,
 #  but different sequences of values
 
+timearr = np.zeros(5)
+
 # Bitgenerator PCG64 (default for numpy), with seed 6
 rng = Generator(PCG64(seed=6))
 print(rng)  # Bitgenerator check
 # Pseudo-random, uniform range between 0-1 with defined seed
+x1starttime = perf_counter()
 x1 = rng.uniform(0, 1, 100)
+x1endtime = perf_counter()
+timearr[0] = x1endtime-x1starttime
 x15 = np.roll(x1, 10)  # Move each value of x1 10 spaces along
 
 # Bitgenerator PCG64 (default for numpy), with seed 15832
 rng = Generator(PCG64(seed=15832))
 print(rng)
 # Pseudo-random, uniform range between 0-1 with defined seed
+x2starttime = perf_counter()
 x2 = rng.uniform(0, 1, 100)
+x2endtime = perf_counter()
+timearr[1] = x2endtime-x2starttime
 x25 = np.roll(x2, 10)
 
 # Seed Comparison
@@ -67,7 +76,7 @@ plt.ylabel('$x_{n+10}$')
 plt.title('PCG64, seed = 15832\n Autocorrelation = {}'.format(
     np.correlate(x2, x25)))
 plt.tight_layout()
-plt.savefig(fname='PCG6415832')
+plt.savefig(fname='PCG64-15832')
 plt.show()
 
 # -------- Using the same seeds on two different generators should result in reproducible,
@@ -77,19 +86,10 @@ plt.show()
 rng = Generator(MT19937(6))
 print(rng)
 # Pseudo-random, uniform range between 0-1 with defined seed
+y1starttime = perf_counter()
 y1 = rng.uniform(0, 1, 100)
-
-# Bitgenerator MT19937 (default for numpy), with seed 5525
-rng = Generator(MT19937(5255))
-# Pseudo-random, uniform range between 0-1 with defined seed
-y1 = rng.uniform(0, 1, 100)
-y15 = np.roll(y1, 10)
-
-# Bitgenerator MT19937 (default for numpy), with seed 7381
-rng = Generator(MT19937(7381))
-# Pseudo-random, uniform range between 0-1 with defined seed
-y2 = rng.uniform(0, 1, 100)
-y25 = np.roll(y2, 10)
+y1endtime = perf_counter()
+timearr[2] = y1endtime-y1starttime
 
 # Different Generator - Same seed Comparison
 plt.figure(figsize=(6, 6))
@@ -100,6 +100,24 @@ plt.title('PCG64-MT19937-6')
 plt.tight_layout()
 plt.savefig(fname='PCG64-MT19937-6')
 plt.show()
+
+# Bitgenerator MT19937 (default for numpy), with seed 5525
+rng = Generator(MT19937(5255))
+# Pseudo-random, uniform range between 0-1 with defined seed
+y1starttime = perf_counter()
+y1 = rng.uniform(0, 1, 100)
+y1endtime = perf_counter()
+timearr[3]=y1endtime-y1starttime
+y15 = np.roll(y1, 10)
+
+# Bitgenerator MT19937 (default for numpy), with seed 7381
+rng = Generator(MT19937(7381))
+# Pseudo-random, uniform range between 0-1 with defined seed
+y2starttime = perf_counter()
+y2 = rng.uniform(0, 1, 100)
+y2endtime = perf_counter()
+timearr[4] = y2endtime-y2starttime
+y25 = np.roll(y2, 10)
 
 # Shifted value comparison ( uniformity )
 plt.figure(figsize=(6, 6))
@@ -122,3 +140,5 @@ plt.title('MT19937, seed = 7381\n Autocorrelation = {}'.format(
 plt.tight_layout()
 plt.savefig(fname='MT19937-7381')
 plt.show()
+
+print('\nTimes: \n PCG64-6 =', timearr[0]/1e-6,'µs\n', 'PCG64-15832 =', timearr[1]/1e-6,'µs\n', 'MT19937-6 =', timearr[2]/1e-6,'µs\n', 'MT19937-5255 =', timearr[3]/1e-6,'µs\n' ,'MT19937-7381 =', timearr[4]/1e-6,'µs\n')
