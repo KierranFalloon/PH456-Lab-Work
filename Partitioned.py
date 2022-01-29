a6faba73510e5d57a18c8114a947b11e79cb0ed4
Kierran Falloon <kgwfalloon@gmail.com>
2022-01-29 20:44:46 +0000

    Partitioned box graphs

--- Partitioned.py
+++ Partitioned.py
@@ -36,39 +36,35 @@ tarray = np.zeros_like(Marray)
 for t in range(time): # simulating 1000s of time
     r = rng.uniform(0, 1) # Random number between 0 and 1
     frac = M/N # As no other factors are considered, this can be thought of as the probability of moving 
-    R = N-M
+    
     if r < frac: # if random number is less than the fraction, particle moves from LHS to RHS
         M = M-1
+        R = N-M
     elif r > frac:
         M = M+1 # otherwise, particle move from RHS to LHS
-    else: # if equilibrium, end as no more movement is possible
-        break
+        R = N-M
+
     Marray[t] = M
     Rarray[t] = R
     tarray[t] = t
 
+plt.figure()
 plt.plot(tarray,Marray,label = 'LHS')
 plt.plot(tarray,Rarray,label = 'RHS')
 plt.xlabel("Time (arb.)")
 plt.ylabel("Number of particles")
 plt.title("Simple partitioned box simulation")
-plt.hlines(50,0,time,color = 'grey', linestyle = '--')
+plt.hlines(0,tarray[0],tarray[time-1], color = 'lightgrey', linestyle = '--')
+plt.hlines(N,tarray[0],tarray[time-1], color = 'lightgrey', linestyle = '--')
 plt.legend()
 plt.savefig("IMAGES/Simple partitioned box")
-plt.show()
+ 
 
-N = 100 # Initial number of particles in the entire box
-M = 100 # initial number of particles on the L.H.S
-time = 1000
-Marray = np.zeros(time)
-Rarray = np.zeros_like(Marray)
-tarray = np.zeros_like(Marray)
-
-def simple_partition(N,M,time,frac):
+def simple_partition(N,M,time,prob):
     
-    rng = Generator(PCG64(seed=3))
     """ Simple partitioned box simulation, where particles movement is dictated only by 
-    the probability of moving to the other side of the partition.
+    the probability of moving to the other side of the partition. This method tracks only the no.
+    of particles on each side of the partition.
     
     Args:
         N: Total number of particles in the box
@@ -77,31 +73,59 @@ def simple_partition(N,M,time,frac):
         frac (0 <= frac <= 1): Probability of a particle moving from the L.H.S to the R.H.S
     """
 
+    funcstarttime = perf_counter()
     Marray = np.zeros(time)
     Rarray = np.zeros_like(Marray)
     tarray = np.zeros_like(Marray)
      
     for t in range(time):
         r = rng.uniform(0, 1)
-        R = N-M # Number of particles on the R.H.S
-        #frac = M/N
-        if r < frac: # if random number is less than the fraction, particle moves from LHS to RHS
-            M = M-1
-        elif r > frac:
-            M = M+1 # otherwise, particle move from RHS to LHS
-        else: # if equilibrium, end as no more movement is possible
-            break
-        
+        R = N-M
+
+        if r < prob: # if random number is less than the fraction, particle moves from LHS to RHS
+            M -= 1
+            R = N-M
+            if M >= N:
+                M = N
+            if R >= N:
+                R = N
+
+            if M <= 0:
+                M = 0 
+            if R <= 0:
+                R = 0
+
+        elif r > prob:
+            M += 1 # otherwise, particle move from RHS to LHS
+            R = N-M
+            if M >= N:
+                M = N
+            if R >= N:
+                R = N
+
+            if M <= 0:
+                M = 0 
+            if R <= 0:
+                R = 0
+
         Marray[t] = M
         Rarray[t] = R
         tarray[t] = t
+    funcendtime = perf_counter()
+    elapsed_time = np.round((funcendtime - funcstarttime)*1e3,4)
+
+    plt.figure()
     plt.plot(tarray,Marray,label = 'LHS')
     plt.plot(tarray,Rarray,label = 'RHS')
+    plt.hlines(0,tarray[0],tarray[time-1], color = 'lightgrey', linestyle = '--')
+    plt.hlines(N,tarray[0],tarray[time-1], color = 'lightgrey', linestyle = '--')
     plt.xlabel("Time (arb.)")
     plt.ylabel("Number of particles")
-    plt.title('Simple partitioned box simulation\n P(LHS $\longrightarrow$ RHS) = {}'.format(frac))
+    plt.title('Simple partitioned box simulation\n P(LHS $\longrightarrow$ RHS) = {}'.format(prob))
     plt.legend()
-    plt.savefig("IMAGES/Simple partitioned box, 75")
+    plt.savefig("IMAGES/Simple partitioned box_{}".format(int(prob*100)))
     plt.show()
+    print("Simulation time = ${}m s$".format(elapsed_time))
 
-simple_partition(100,100,1000,0.75)
+for i in range(3):
+    simple_partition(100,100,1000,0.25*(i+1))
