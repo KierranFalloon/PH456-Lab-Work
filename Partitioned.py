e4d83dd1dbfd0230439b600bb90ac59aad952e3b
Kierran Falloon <kgwfalloon@gmail.com>
2022-02-01 11:48:44 +0000

    Pep8 formatting
    Autoformatted using black, needs checking over

--- Partitioned.py
+++ Partitioned.py
@@ -4,13 +4,16 @@ import matplotlib.pyplot as plt
 from time import perf_counter
 from scipy.stats import chisquare
 
+
 def chisquare_test(array, bins):
-    f_observed, edges = np.histogram(array, bins) # Split input array into M equal bins
-    f_expected = np.ones(bins)*( len(array) / bins ) # Assume equal spacing in bins
-    chi_square_array = [0]*bins # Placeholding empty array
+    f_observed, edges = np.histogram(array, bins)  # Split input array into M equal bins
+    f_expected = np.ones(bins) * (len(array) / bins)  # Assume equal spacing in bins
+    chi_square_array = [0] * bins  # Placeholding empty array
     for i in range(len(array)):
-        chi_square_array[i] = ( (f_observed[i] - f_expected[i])**2 / f_expected[i]) # chi square equation
-    
+        chi_square_array[i] = (f_observed[i] - f_expected[i]) ** 2 / f_expected[
+            i
+        ]  # chi square equation
+
     return np.sum(chi_square_array)
 
     """Write a program to simulate a partitioned box containing N particles,
@@ -19,53 +22,58 @@ any one particle moving from one side of the partition to the other in
 unit time. Present your results graphically as well as textually.
     """
 
-#Generate a random number r from a uniformly distributed set of random numbers in the interval 0 ≤ r < 1.
-#Compare r to the current value of the fraction of particles n/N on the left side of the box.
-#If r < n/N, move a particle from left to right, that is, let n → n - 1; otherwise, move a particle from right to left.
-#Increase the time by 1.
+
+# Generate a random number r from a uniformly distributed set of random numbers in the interval 0 ≤ r < 1.
+# Compare r to the current value of the fraction of particles n/N on the left side of the box.
+# If r < n/N, move a particle from left to right, that is, let n → n - 1; otherwise, move a particle from right to left.
+# Increase the time by 1.
 
 rng = Generator(PCG64(seed=3))
 
-N = 100 # Initial number of particles in the entire box
-M = 100 # initial number of particles on the L.H.S
+N = 100  # Initial number of particles in the entire box
+M = 100  # initial number of particles on the L.H.S
 time = 1000
 Marray = np.zeros(time)
 Rarray = np.zeros_like(Marray)
 tarray = np.zeros_like(Marray)
 
-for t in range(time): # simulating 1000s of time
-    r = rng.uniform(0, 1) # Random number between 0 and 1
-    frac = M/N # As no other factors are considered, this can be thought of as the probability of moving 
-    
-    if r < frac: # if random number is less than the fraction, particle moves from LHS to RHS
-        M = M-1
-        R = N-M
+for t in range(time):  # simulating 1000s of time
+    r = rng.uniform(0, 1)  # Random number between 0 and 1
+    frac = (
+        M / N
+    )  # As no other factors are considered, this can be thought of as the probability of moving
+
+    if (
+        r < frac
+    ):  # if random number is less than the fraction, particle moves from LHS to RHS
+        M = M - 1
+        R = N - M
     elif r > frac:
-        M = M+1 # otherwise, particle move from RHS to LHS
-        R = N-M
+        M = M + 1  # otherwise, particle move from RHS to LHS
+        R = N - M
 
     Marray[t] = M
     Rarray[t] = R
     tarray[t] = t
 
 plt.figure()
-plt.plot(tarray,Marray,label = 'LHS')
-plt.plot(tarray,Rarray,label = 'RHS')
+plt.plot(tarray, Marray, label="LHS")
+plt.plot(tarray, Rarray, label="RHS")
 plt.xlabel("Time (arb.)")
 plt.ylabel("Number of particles")
 plt.title("Simple partitioned box simulation")
-plt.hlines(0,tarray[0],tarray[time-1], color = 'lightgrey', linestyle = '--')
-plt.hlines(N,tarray[0],tarray[time-1], color = 'lightgrey', linestyle = '--')
+plt.hlines(0, tarray[0], tarray[time - 1], color="lightgrey", linestyle="--")
+plt.hlines(N, tarray[0], tarray[time - 1], color="lightgrey", linestyle="--")
 plt.legend()
 plt.savefig("IMAGES/Simple partitioned box")
- 
 
-def simple_partition(N,M,time,prob):
-    
-    """ Simple partitioned box simulation, where particles movement is dictated only by 
+
+def simple_partition(N, M, time, prob):
+
+    """Simple partitioned box simulation, where particles movement is dictated only by
     the probability of moving to the other side of the partition. This method tracks only the no.
     of particles on each side of the partition.
-    
+
     Args:
         N: Total number of particles in the box
         M: Number of particles initially on the L.H.S
@@ -77,34 +85,36 @@ def simple_partition(N,M,time,prob):
     Marray = np.zeros(time)
     Rarray = np.zeros_like(Marray)
     tarray = np.zeros_like(Marray)
-     
+
     for t in range(time):
         r = rng.uniform(0, 1)
-        R = N-M
+        R = N - M
 
-        if r < prob: # if random number is less than the fraction, particle moves from LHS to RHS
+        if (
+            r < prob
+        ):  # if random number is less than the fraction, particle moves from LHS to RHS
             M -= 1
-            R = N-M
+            R = N - M
             if M >= N:
                 M = N
             if R >= N:
                 R = N
 
             if M <= 0:
-                M = 0 
+                M = 0
             if R <= 0:
                 R = 0
 
         elif r > prob:
-            M += 1 # otherwise, particle move from RHS to LHS
-            R = N-M
+            M += 1  # otherwise, particle move from RHS to LHS
+            R = N - M
             if M >= N:
                 M = N
             if R >= N:
                 R = N
 
             if M <= 0:
-                M = 0 
+                M = 0
             if R <= 0:
                 R = 0
 
@@ -112,23 +122,26 @@ def simple_partition(N,M,time,prob):
         Rarray[t] = R
         tarray[t] = t
     funcendtime = perf_counter()
-    elapsed_time = np.round((funcendtime - funcstarttime)*1e3,4)
+    elapsed_time = np.round((funcendtime - funcstarttime) * 1e3, 4)
 
     plt.figure()
-    plt.plot(tarray,Marray,label = 'LHS')
-    plt.plot(tarray,Rarray,label = 'RHS')
-    plt.hlines(0,tarray[0],tarray[time-1], color = 'lightgrey', linestyle = '--')
-    plt.hlines(N,tarray[0],tarray[time-1], color = 'lightgrey', linestyle = '--')
+    plt.plot(tarray, Marray, label="LHS")
+    plt.plot(tarray, Rarray, label="RHS")
+    plt.hlines(0, tarray[0], tarray[time - 1], color="lightgrey", linestyle="--")
+    plt.hlines(N, tarray[0], tarray[time - 1], color="lightgrey", linestyle="--")
     plt.xlabel("Time (arb.)")
     plt.ylabel("Number of particles")
-    plt.suptitle('Simple partitioned box simulation\n MT19937, seed = {}\n'.format(seed))
-    plt.title('\nP(LHS $\longrightarrow$ RHS) = {}'.format(prob), fontsize = 9)
+    plt.suptitle(
+        "Simple partitioned box simulation\n MT19937, seed = {}\n".format(seed)
+    )
+    plt.title("\nP(LHS $\longrightarrow$ RHS) = {}".format(prob), fontsize=9)
     plt.legend()
-    plt.savefig("IMAGES/Simple partitioned box_{}MT19937".format(int(prob*100)))
+    plt.savefig("IMAGES/Simple partitioned box_{}MT19937".format(int(prob * 100)))
     plt.show()
     print("Simulation time = ${}ms$".format(elapsed_time))
+
 
 seed = 3
 rng = Generator(MT19937(seed=seed))
 for i in range(3):
-    simple_partition(100,100,1000,0.25*(i+1))
+    simple_partition(100, 100, 1000, 0.25 * (i + 1))
--- RNG_comp.py
+++ RNG_comp.py
@@ -11,13 +11,27 @@ import matplotlib.pyplot as plt
 from time import perf_counter
 from scipy.stats import chisquare
 
+
 def chisquare_test(array, bins):
-    f_observed, edges = np.histogram(array, bins) # Split input array into M equal bins
-    f_expected = np.ones(bins)*( len(array) / bins ) # Assume equal spacing in bins
-    chi_square_array = [0]*bins # Placeholding empty array
+
+    """Chi-squared test
+
+    Args:
+        array: Array to be tested
+        bins: Number of bins 'array' is to be split into
+
+    Returns:
+        [float]: Chi-squared value
+    """
+
+    f_observed, edges = np.histogram(array, bins)  # Split input array into M equal bins
+    f_expected = np.ones(bins) * (len(array) / bins)  # Assume equal spacing in bins
+    chi_square_array = [0] * bins  # Placeholding empty array
     for i in range(len(array)):
-        chi_square_array[i] = ( (f_observed[i] - f_expected[i])**2 / f_expected[i]) # chi square equation
-    
+        chi_square_array[i] = (f_observed[i] - f_expected[i]) ** 2 / f_expected[
+            i
+        ]  # chi square equation
+
     return np.sum(chi_square_array)
 
     """Using different initial seeds and at least two different pseudo-random
@@ -25,6 +39,7 @@ generators, produce sequences of uniformly distributed random numbers.
 Test these values for a) uniformity and b) lack of sequential
 correlation. Present your analysis as graphically as possible.
     """
+
 
 # -------- Using two different seeds on the same generator should result in reproducible,
 #  but different sequences of values
@@ -37,9 +52,9 @@ rng = Generator(PCG64(seed=6))
 x1starttime = perf_counter()
 x1 = rng.uniform(0, 1, 2000)
 x1endtime = perf_counter()
-timearr[0] = x1endtime-x1starttime
+timearr[0] = x1endtime - x1starttime
 x15 = np.roll(x1, 10)  # Move each value of x1 10 spaces along
-pcg64chi1 = chisquare_test(x1,2000)
+pcg64chi1 = chisquare_test(x1, 2000)
 
 # Bitgenerator PCG64 (default for numpy), with seed 15832
 rng = Generator(PCG64(seed=15832))
@@ -47,44 +62,52 @@ rng = Generator(PCG64(seed=15832))
 x2starttime = perf_counter()
 x2 = rng.uniform(0, 1, 2000)
 x2endtime = perf_counter()
-timearr[1] = x2endtime-x2starttime
+timearr[1] = x2endtime - x2starttime
 x25 = np.roll(x2, 10)
-pcg64chi2 = chisquare_test(x2,2000)
-pcg64chi3 = chisquare_test(x15,2000)
-pcg64chi4 = chisquare_test(x25,2000)
-correl1 = np.around(np.correlate(x1, x15)[0],2)
-correl2 = np.around(np.correlate(x2, x25)[0],2)
-pearsoncoeff1 = np.around(np.corrcoef(x1,x15)[0,1],5)
-pearsoncoeff2 = np.around(np.corrcoef(x2,x25)[0,1],5)
+pcg64chi2 = chisquare_test(x2, 2000)
+pcg64chi3 = chisquare_test(x15, 2000)
+pcg64chi4 = chisquare_test(x25, 2000)
+correl1 = np.around(np.correlate(x1, x15)[0], 2)
+correl2 = np.around(np.correlate(x2, x25)[0], 2)
+pearsoncoeff1 = np.around(np.corrcoef(x1, x15)[0, 1], 5)
+pearsoncoeff2 = np.around(np.corrcoef(x2, x25)[0, 1], 5)
 
 # Seed Comparison
 plt.figure(figsize=(6, 6))
-plt.scatter(x1, x2, marker='x')
-plt.xlabel('Seed = 6, $\chi^2 = {}$'.format(pcg64chi1))
-plt.ylabel('Seed = 15832, $\chi^2 = {}$'.format(pcg64chi2))
-plt.title('PCG64\n')
+plt.scatter(x1, x2, marker="x")
+plt.xlabel("Seed = 6, $\chi^2 = {}$".format(pcg64chi1))
+plt.ylabel("Seed = 15832, $\chi^2 = {}$".format(pcg64chi2))
+plt.title("PCG64\n")
 plt.tight_layout()
-plt.savefig(fname='IMAGES/PCG64-6-15832')
+plt.savefig(fname="IMAGES/PCG64-6-15832")
 
 # Shifted value comparison ( uniformity )
 plt.figure(figsize=(6, 6))
-plt.scatter(x1, x15, marker='x')
-plt.xlabel('$x_n$')
-plt.ylabel('$x_{n+10}$')
-plt.suptitle('PCG64, seed = 6')
-plt.title(r'Cross-correlation $\approx %1.2f$, $\chi^2 \approx %s, \rho_{XY}\approx %1.4f$' %(correl1, pcg64chi1, pearsoncoeff1), fontsize = 10)
+plt.scatter(x1, x15, marker="x")
+plt.xlabel("$x_n$")
+plt.ylabel("$x_{n+10}$")
+plt.suptitle("PCG64, seed = 6")
+plt.title(
+    r"Cross-correlation $\approx %1.2f$, $\chi^2 \approx %s, \rho_{XY}\approx %1.4f$"
+    % (correl1, pcg64chi1, pearsoncoeff1),
+    fontsize=10,
+)
 plt.tight_layout()
-plt.savefig(fname='IMAGES/PCG64-6')
+plt.savefig(fname="IMAGES/PCG64-6")
 
 # Shifted value comparison ( uniformity )
 plt.figure(figsize=(6, 6))
-plt.scatter(x2, x25, marker='x')
-plt.xlabel('$x_n$')
-plt.ylabel('$x_{n+10}$')
-plt.suptitle('PCG64, seed = 15832')
-plt.title(r'Cross-correlation $\approx %1.2f$, $\chi^2 \approx %s, \rho_{XY}\approx %1.4f$' %(correl2, pcg64chi2, pearsoncoeff2), fontsize = 10)
+plt.scatter(x2, x25, marker="x")
+plt.xlabel("$x_n$")
+plt.ylabel("$x_{n+10}$")
+plt.suptitle("PCG64, seed = 15832")
+plt.title(
+    r"Cross-correlation $\approx %1.2f$, $\chi^2 \approx %s, \rho_{XY}\approx %1.4f$"
+    % (correl2, pcg64chi2, pearsoncoeff2),
+    fontsize=10,
+)
 plt.tight_layout()
-plt.savefig(fname='IMAGES/PCG64-15832')
+plt.savefig(fname="IMAGES/PCG64-15832")
 
 
 # -------- Using the same seeds on two different generators should result in reproducible,
@@ -96,17 +119,17 @@ rng = Generator(MT19937(6))
 y1starttime = perf_counter()
 y1 = rng.uniform(0, 1, 2000)
 y1endtime = perf_counter()
-timearr[2] = y1endtime-y1starttime
-mt19937chi1 = chisquare_test(y1,2000)
+timearr[2] = y1endtime - y1starttime
+mt19937chi1 = chisquare_test(y1, 2000)
 
 # Different Generator - Same seed Comparison
 plt.figure(figsize=(6, 6))
-plt.scatter(x1, y1, marker='x')
-plt.xlabel('PCG64, seed = 6, $\chi^2 = {}$'.format(pcg64chi1))
-plt.ylabel('MT19937, seed = 6, $\chi^2 = {}$'.format(mt19937chi1))
-plt.title('PCG64-MT19937-6\n')
+plt.scatter(x1, y1, marker="x")
+plt.xlabel("PCG64, seed = 6, $\chi^2 = {}$".format(pcg64chi1))
+plt.ylabel("MT19937, seed = 6, $\chi^2 = {}$".format(mt19937chi1))
+plt.title("PCG64-MT19937-6\n")
 plt.tight_layout()
-plt.savefig(fname='IMAGES/PCG64-MT19937-6')
+plt.savefig(fname="IMAGES/PCG64-MT19937-6")
 
 # Bitgenerator MT19937 (default for numpy), with seed 5525
 rng = Generator(MT19937(5255))
@@ -114,9 +137,9 @@ rng = Generator(MT19937(5255))
 y1starttime = perf_counter()
 y1 = rng.uniform(0, 1, 2000)
 y1endtime = perf_counter()
-timearr[3]=y1endtime-y1starttime
+timearr[3] = y1endtime - y1starttime
 y15 = np.roll(y1, 10)
-mt19937chi1 = chisquare_test(y1,2000)
+mt19937chi1 = chisquare_test(y1, 2000)
 
 # Bitgenerator MT19937 (default for numpy), with seed 7381
 rng = Generator(MT19937(7381))
@@ -124,50 +147,73 @@ rng = Generator(MT19937(7381))
 y2starttime = perf_counter()
 y2 = rng.uniform(0, 1, 2000)
 y2endtime = perf_counter()
-timearr[4] = y2endtime-y2starttime
+timearr[4] = y2endtime - y2starttime
 y25 = np.roll(y2, 10)
-mt19937chi2 = chisquare_test(y2,2000)
-mt19937chi3 = chisquare_test(y15,2000)
-mt19937chi4 = chisquare_test(y25,2000) 
-correl3 = np.around(np.correlate(y1, y15)[0],2)
-correl4 = np.around(np.correlate(y2, y25)[0],2)
-pearsoncoeff3 = np.around(np.corrcoef(y1,y15)[0,1],5)
-pearsoncoeff4 = np.around(np.corrcoef(y2,y25)[0,1],5)
-
+mt19937chi2 = chisquare_test(y2, 2000)
+mt19937chi3 = chisquare_test(y15, 2000)
+mt19937chi4 = chisquare_test(y25, 2000)
+correl3 = np.around(np.correlate(y1, y15)[0], 2)
+correl4 = np.around(np.correlate(y2, y25)[0], 2)
+pearsoncoeff3 = np.around(np.corrcoef(y1, y15)[0, 1], 5)
+pearsoncoeff4 = np.around(np.corrcoef(y2, y25)[0, 1], 5)
 
 
 # Seed Comparison
 plt.figure(figsize=(6, 6))
-plt.scatter(y1, y2, marker='x')
-plt.xlabel('Seed = 5255, $\chi^2 = {}$'.format(mt19937chi1))
-plt.ylabel('Seed = 7381, $\chi^2 = {}$'.format(mt19937chi2))
-plt.title('MT19937\n')
+plt.scatter(y1, y2, marker="x")
+plt.xlabel("Seed = 5255, $\chi^2 = {}$".format(mt19937chi1))
+plt.ylabel("Seed = 7381, $\chi^2 = {}$".format(mt19937chi2))
+plt.title("MT19937\n")
 plt.tight_layout()
-plt.savefig(fname='IMAGES/MT19937-5525-7381')
+plt.savefig(fname="IMAGES/MT19937-5525-7381")
 
 # Shifted value comparison ( uniformity )
 plt.figure(figsize=(6, 6))
-plt.scatter(y1, y15, marker='x')
-plt.xlabel('$x_n$')
-plt.ylabel('$x_{n+10}$')
-plt.suptitle('MT19937, seed = 5255')
-plt.title(r'Cross-correlation $\approx %1.2f$, $\chi^2 \approx %s, \rho_{XY}\approx %1.4f$' %(correl3, mt19937chi1, pearsoncoeff3), fontsize = 10)
+plt.scatter(y1, y15, marker="x")
+plt.xlabel("$x_n$")
+plt.ylabel("$x_{n+10}$")
+plt.suptitle("MT19937, seed = 5255")
+plt.title(
+    r"Cross-correlation $\approx %1.2f$, $\chi^2 \approx %s, \rho_{XY}\approx %1.4f$"
+    % (correl3, mt19937chi1, pearsoncoeff3),
+    fontsize=10,
+)
 plt.tight_layout()
-plt.savefig(fname='IMAGES/MT19937-5255')
+plt.savefig(fname="IMAGES/MT19937-5255")
 
 # Shifted value comparison ( uniformity )
 plt.figure(figsize=(6, 6))
-plt.scatter(y2, y25, marker='x')
-plt.xlabel('$x_n$')
-plt.ylabel('$x_{n+10}$')
-plt.suptitle('MT19937, seed = 7381')
-plt.title(r'Cross-correlation $\approx %1.2f$, $\chi^2 \approx %s, \rho_{XY}\approx %1.4f$' %(correl4, mt19937chi2, pearsoncoeff4), fontsize = 10)
+plt.scatter(y2, y25, marker="x")
+plt.xlabel("$x_n$")
+plt.ylabel("$x_{n+10}$")
+plt.suptitle("MT19937, seed = 7381")
+plt.title(
+    r"Cross-correlation $\approx %1.2f$, $\chi^2 \approx %s, \rho_{XY}\approx %1.4f$"
+    % (correl4, mt19937chi2, pearsoncoeff4),
+    fontsize=10,
+)
 plt.tight_layout()
-plt.savefig(fname='IMAGES/MT19937-7381')
-print('\nTimes: \n PCG64-6 =', timearr[0]/1e-6,'µs\n', 'PCG64-15832 =', timearr[1]/1e-6,'µs\n', 'MT19937-6 =', timearr[2]/1e-6,'µs\n', 'MT19937-5255 =', timearr[3]/1e-6,'µs\n' ,'MT19937-7381 =', timearr[4]/1e-6,'µs\n')
+plt.savefig(fname="IMAGES/MT19937-7381")
+print(
+    "\nTimes: \n PCG64-6 =",
+    timearr[0] / 1e-6,
+    "µs\n",
+    "PCG64-15832 =",
+    timearr[1] / 1e-6,
+    "µs\n",
+    "MT19937-6 =",
+    timearr[2] / 1e-6,
+    "µs\n",
+    "MT19937-5255 =",
+    timearr[3] / 1e-6,
+    "µs\n",
+    "MT19937-7381 =",
+    timearr[4] / 1e-6,
+    "µs\n",
+)
 
 # Cross-correlation relation
-shift = np.arange(0,1000,1)
+shift = np.arange(0, 1000, 1)
 
 rng = Generator(PCG64(2010))
 # Pseudo-random, uniform range between 0-1 with defined seed
@@ -177,9 +223,9 @@ z1 = rng.uniform(0, 1, 1000)
 pearsoncoeff5 = np.zeros_like(correlation1)
 for i in range(1000):
     z15 = np.roll(z1, shift[i])
-    correlation1[i] = np.correlate(z1,z15)
-    pcg642010chi[i] = chisquare_test(z15,1000)
-    pearsoncoeff5[i] = np.around(np.corrcoef(z1,z15)[0,1],5)
+    correlation1[i] = np.correlate(z1, z15)
+    pcg642010chi[i] = chisquare_test(z15, 1000)
+    pearsoncoeff5[i] = np.around(np.corrcoef(z1, z15)[0, 1], 5)
 
 rng = Generator(MT19937(2010))
 # Pseudo-random, uniform range between 0-1 with defined seed
@@ -189,26 +235,48 @@ pearsoncoeff6 = np.zeros_like(correlation2)
 z2 = rng.uniform(0, 1, 1000)
 for i in range(1000):
     z25 = np.roll(z2, shift[i])
-    correlation2[i] = np.correlate(z2,z25)
-    mt199372010chi[i] = chisquare_test(z25,1000)
-    pearsoncoeff6[i] = np.around(np.corrcoef(z2,z25)[0,1],5)
+    correlation2[i] = np.correlate(z2, z25)
+    mt199372010chi[i] = chisquare_test(z25, 1000)
+    pearsoncoeff6[i] = np.around(np.corrcoef(z2, z25)[0, 1], 5)
 
-wiggle1 = np.round(correlation1[1:1000].max()-correlation1.min(),2)
-wiggle2 = np.round(correlation2[1:1000].max()-correlation2.min(),2)
-std1 = np.round(np.std(correlation1[1:1000]),2)
-std2 = np.round(np.std(correlation2[1:1000]),2)
+wiggle1 = np.round(correlation1[1:1000].max() - correlation1.min(), 2)
+wiggle2 = np.round(correlation2[1:1000].max() - correlation2.min(), 2)
+std1 = np.round(np.std(correlation1[1:1000]), 2)
+std2 = np.round(np.std(correlation2[1:1000]), 2)
 
-fig,(ax1,ax2)= plt.subplots(2,1, sharex = True)
-ax1.plot(shift,correlation1)
+fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+ax1.plot(shift, correlation1)
 ax1.set_title("PCG64")
-ax1.text(shift[5],correlation1.max()-5, "Max = {}".format(np.round(correlation1.max(),2)))
-ax1.hlines(np.mean(correlation1[1:1000]),0,1000,'r', '--', label = "$\mu = {}$,\n$\sigma = {}$,\n$\Delta = {}$".format(np.round(np.mean(correlation1[1:1000]),2),std1,wiggle1))
-ax1.text(1002,231,"$\Delta$")
-ax2.plot(shift,correlation2)
+ax1.text(
+    shift[5], correlation1.max() - 5, "Max = {}".format(np.round(correlation1.max(), 2))
+)
+ax1.hlines(
+    np.mean(correlation1[1:1000]),
+    0,
+    1000,
+    "r",
+    "--",
+    label="$\mu = {}$,\n$\sigma = {}$,\n$\Delta = {}$".format(
+        np.round(np.mean(correlation1[1:1000]), 2), std1, wiggle1
+    ),
+)
+ax1.text(1002, 231, "$\Delta$")
+ax2.plot(shift, correlation2)
 ax2.set_title("MT19937")
-ax2.text(shift[5],correlation2.max()-5, "Max = {}".format(np.round(correlation2.max(),2)))
-ax2.hlines(np.mean(correlation2[1:1000]),0,1000,'r', '-.', label = "$\mu = {}$,\n$\sigma = {}$,\n$\Delta = {}$".format(np.round(np.mean(correlation2[1:1000]),2),std2,wiggle2))
-ax2.text(1002,250,"$\Delta$")
+ax2.text(
+    shift[5], correlation2.max() - 5, "Max = {}".format(np.round(correlation2.max(), 2))
+)
+ax2.hlines(
+    np.mean(correlation2[1:1000]),
+    0,
+    1000,
+    "r",
+    "-.",
+    label="$\mu = {}$,\n$\sigma = {}$,\n$\Delta = {}$".format(
+        np.round(np.mean(correlation2[1:1000]), 2), std2, wiggle2
+    ),
+)
+ax2.text(1002, 250, "$\Delta$")
 fig.suptitle("Seed = 2010")
 ax2.set_xlabel("Shifted index $i$")
 fig.supylabel("Cross-correlation between $x_n~&~x_{n+i}$")
@@ -217,10 +285,10 @@ ax2.legend()
 plt.tight_layout()
 plt.savefig("IMAGES/Correlation-shift-comparison")
 
-fig2,(ax1,ax2)= plt.subplots(2,1, sharex = True)
-ax1.plot(shift,pearsoncoeff5)
+fig2, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+ax1.plot(shift, pearsoncoeff5)
 ax1.set_title("PCG64")
-ax2.plot(shift,pearsoncoeff6)
+ax2.plot(shift, pearsoncoeff6)
 ax2.set_title("MT19937")
 fig2.suptitle("Seed = 2010")
 ax2.set_xlabel("Shifted index $i$")
