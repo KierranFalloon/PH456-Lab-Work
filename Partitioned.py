4a28b5118a3c0bb31e5a732e673a179366bbb463
Kierran Falloon <kgwfalloon@gmail.com>
2022-02-07 13:31:22 +0000

    Update Partitioned.py

--- Partitioned.py
+++ Partitioned.py
@@ -34,8 +34,8 @@ def simple_partition(N, M, time):
     plt.figure()
 
     """Simple partitioned box simulation, where particles movement is dictated only by
-    the probability of moving to the other side of the partition. This method takes input of
-    probability of passing from LHS to RHS.
+    the probability of moving to the other side of the partition. This method uses arrays that
+    track particle positions.
 
     Args:
         N: Total number of particles in the box
@@ -66,9 +66,7 @@ def simple_partition(N, M, time):
     plt.suptitle('Lecture example, PCG64')
     plt.legend()
     plt.tight_layout()
-    plt.show()
-    plt.savefig("IMAGES/Simple partitioned box PCG64")
-    print("Simulation time = ${}ms$".format(elapsed_time))
+    print(r"Simulation time = ${}ms$".format(elapsed_time))
 
 def simple_partition_PROB(N, M, time, prob):
 
@@ -80,9 +78,9 @@ def simple_partition_PROB(N, M, time, prob):
 
     Args:
         N: Total number of particles in the box
-        M: Number of particles initially on the L.H.S
+        M: Number of particles on the L.H.S
         time: Length of time (arb. units)
-        frac (0 <= frac <= 1): Probability of a particle moving from the L.H.S to the R.H.S
+        prob (0 <= prob <= 1): Desired probability of a particle moving from the L.H.S to the R.H.S
     """
 
     funcstarttime = perf_counter()
@@ -91,24 +89,27 @@ def simple_partition_PROB(N, M, time, prob):
     tarray = np.arange(0,time,1)
     Msumarray = np.zeros(time)
     Rsumarray = np.zeros(time)
-
+    
     for i in range(time):
-        r = rng.integers(1,N,1)[0]
-        Marray[r], Rarray[r] = Rarray[r], Marray[r]
-        Msumarray[i], Rsumarray[i] = Marray.sum(), Rarray.sum()
-
-    plt.plot(tarray, Msumarray, label = 'LHS')
-    plt.plot(tarray, Rsumarray, label = 'RHS')
+        r = rng.random() # Create random float between 0 and 1
+        index = rng.choice(M,1) # Create random index choice
+        if 0 < r < prob: # If random float is within probability of moving
+            Marray[index], Rarray[index] = 0 , 1 # Move a particle from LHS to RHS
+        else: 
+            Marray[index], Rarray[index] = 1 , 0 # Move a particle from RHS to LHS
+        Msumarray[i], Rsumarray[i] = Marray.sum(), Rarray.sum() 
+        # Sum particle no.s at each step for plotting
     funcendtime = perf_counter()
     elapsed_time = np.round((funcendtime - funcstarttime) * 1e3, 4)
 
+    plt.plot(tarray, Msumarray, label = 'LHS')
+    plt.plot(tarray, Rsumarray, label = 'RHS')
     plt.xlabel("Time (arb.)")
     plt.ylabel("Number of particles")
-    plt.suptitle('Lecture example, PCG64')
-    plt.savefig("IMAGES/Simple partitioned box PCG64")
-    plt.show()
+    plt.suptitle('PCG64')
     plt.legend()
     plt.tight_layout()
-    print("Simulation time = ${}ms$".format(elapsed_time))
+    plt.show()
+    print(r"Simulation time = ${}ms$".format(elapsed_time))
 
-simple_partition(1000,1000,10000)
+simple_partition_PROB(100,100,1000,0.75)
