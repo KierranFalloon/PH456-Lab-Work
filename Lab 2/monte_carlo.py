8db5655e35cda3ecfe98cb8a32aa7820ff9d16a9
Kierran Falloon <kgwfalloon@gmail.com>
2022-02-21 20:33:49 +0000

    Some linting changes

--- Lab 2/monte_carlo.py
+++ Lab 2/monte_carlo.py
@@ -1,10 +1,10 @@
 import math
+from time import perf_counter
 import numpy as np
 import matplotlib.pyplot as plt
-from time import perf_counter
 
-def MonteCarlo_Integrator(func, lim_array, sample_points):
-    
+def montecarlo_integrator(func, lim_array, sample_points):
+
     """ Integrates input function via Monte Carlo methods
 
     Args:
@@ -15,29 +15,31 @@ def MonteCarlo_Integrator(func, lim_array, sample_points):
     Returns:
         integral, variance (float): The estimated integral and variance in the estimate
     """
-    
     start_time = perf_counter() # Timing the function
     dimensions = int(len(lim_array) / 2) # Determines the number of integrals
     func_vals, var_vals = 0, 0
-    rand_vals = np.empty((sample_points, dimensions)) # Empty random vals matrix size (sample_points rows, dimensions columns)
-    
+    rand_vals = np.empty((sample_points, dimensions)) 
+    # Empty random vals matrix size (sample_points rows, dimensions columns)
+
     j=0 # Dimensions counter
     k=0 # Another counter for indexing
     area = 1 # area initial value
-    
+
     while j <= dimensions+(dimensions-1): # Loop through limits via auto counting dimensions
-        area *= lim_array[j+1] - lim_array[j] 
+        area *= lim_array[j+1] - lim_array[j]
         # area = Previous difference in limits * this one, for all limits
         for i in range(sample_points): # Loop through number of points
             rand_vals[i, k] = np.random.uniform(lim_array[j], lim_array[j+1], 1)
             # Create random number and store it in the array, such that each random number
             # is within the input limits
-        j+=2 # Always of form [lower, upper, lower, upper, ...] so +2 to get to next lower limit, 
+        j+=2 # Always of form [lower, upper, lower, upper, ...] so +2 to get to next lower limit
         k+=1 # Index just for assigning rand_vals array
 
     for i in range(sample_points): # Loop through number of points again
-        func_vals += func(*rand_vals[i]) # Determine sum for function value by unpacking each random [xi,yi,zi]
-        var_vals += (func(*rand_vals[i]))**2 # Determine sum for variance values similarly
+        func_vals += func(*rand_vals[i]) 
+        # Determine sum for function value by unpacking each random [xi,yi,zi]
+        var_vals += (func(*rand_vals[i]))**2 
+        # Determine sum for variance values similarly
     integral = area/sample_points * func_vals
     # Collate terms in integration estimation
     variance = ( 1/sample_points * ((var_vals) - (func_vals**2)) )
@@ -60,21 +62,21 @@ def function3(x):
 def function4(x,y):
     return x*y + x
 
-integral1, variance1, rms1, time1 = MonteCarlo_Integrator(function1,[0,1],10000)
+integral1, variance1, rms1, time1 = montecarlo_integrator(function1,[0,1],10000)
 print(integral1, variance1, rms1, time1)
 
-integral2, variance2, rms2, time2 = MonteCarlo_Integrator(function2,[0,1],10000)
+integral2, variance2, rms2, time2 = montecarlo_integrator(function2,[0,1],10000)
 print(integral2, variance2, rms2, time2)
 
-integral3, variance3, rms3, time3 = MonteCarlo_Integrator(function3,[-2,2],10000)
+integral3, variance3, rms3, time3 = montecarlo_integrator(function3,[-2,2],10000)
 print(integral3, variance3, rms3, time3)
 
-integral4, variance4, rms4, time4 = MonteCarlo_Integrator(function4, [0,1,0,1], 10000)
+integral4, variance4, rms4, time4 = montecarlo_integrator(function4, [0,1,0,1], 10000)
 print(integral4, variance4, rms4, time4)
 
 # Use your subroutine to evaluate the size of the region enclosed
 # within an n-sphere of radius 2.0, for n = 3 (i.e. the volume of a ball of
-# radius 1.5) and n = 5.
+# radius 1.5) and n=5.
 
 def sphere_func_3(x,y,z):
     
@@ -90,8 +92,7 @@ def sphere_func_5(x,y,z,rho,epsilon):
     else:
         return 0
 
-integral5, variance5, rms5, time5 = MonteCarlo_Integrator(sphere_func_3, [-2,2,-2,2,-2,2], 10000)
-integral6, variance6, rms6, time6 = MonteCarlo_Integrator(sphere_func_5, [-2,2,-2,2,-2,2,-2,2,-2,2], 10000)
+integral5, variance5, rms5, time5 = montecarlo_integrator(sphere_func_3, [-2,2,-2,2,-2,2], 10000)
+integral6, variance6, rms6, time6 = montecarlo_integrator(sphere_func_5, [-2,2,-2,2,-2,2,-2,2,-2,2], 10000)
 print(integral5, variance5, rms5, time5)
 print(integral6, variance6, rms6, time6)
-
