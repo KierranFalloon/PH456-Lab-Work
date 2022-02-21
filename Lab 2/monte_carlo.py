087052035dceed0e4d37cc37deb8bd9bd286255b
Kierran Falloon <kgwfalloon@gmail.com>
2022-02-21 20:27:10 +0000

    Sphere calculations

--- Lab 2/monte_carlo.py
+++ Lab 2/monte_carlo.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+from time import perf_counter
 
 def MonteCarlo_Integrator(func, lim_array, sample_points):
     
@@ -15,6 +16,7 @@ def MonteCarlo_Integrator(func, lim_array, sample_points):
         integral, variance (float): The estimated integral and variance in the estimate
     """
     
+    start_time = perf_counter() # Timing the function
     dimensions = int(len(lim_array) / 2) # Determines the number of integrals
     func_vals, var_vals = 0, 0
     rand_vals = np.empty((sample_points, dimensions)) # Empty random vals matrix size (sample_points rows, dimensions columns)
@@ -38,12 +40,13 @@ def MonteCarlo_Integrator(func, lim_array, sample_points):
         var_vals += (func(*rand_vals[i]))**2 # Determine sum for variance values similarly
     integral = area/sample_points * func_vals
     # Collate terms in integration estimation
-    print()
     variance = ( 1/sample_points * ((var_vals) - (func_vals**2)) )
     root_mean_squared = np.sqrt(variance)
     # Collate terms in variance of integration estimation
+    end_time = perf_counter()
+    time = end_time - start_time
     
-    return integral, variance, root_mean_squared # Return values
+    return integral, variance, root_mean_squared, time # Return values
 
 def function1(x):
     return 2
@@ -57,17 +60,17 @@ def function3(x):
 def function4(x,y):
     return x*y + x
 
-integral1, variance1, rms1 = MonteCarlo_Integrator(function1,[0,1],10000)
-print(integral1, variance1, rms1)
+integral1, variance1, rms1, time1 = MonteCarlo_Integrator(function1,[0,1],10000)
+print(integral1, variance1, rms1, time1)
 
-integral2, variance2, rms2 = MonteCarlo_Integrator(function2,[0,1],10000)
-print(integral2, variance2, rms2)
+integral2, variance2, rms2, time2 = MonteCarlo_Integrator(function2,[0,1],10000)
+print(integral2, variance2, rms2, time2)
 
-integral3, variance3, rms3 = MonteCarlo_Integrator(function3,[-2,2],10000)
-print(integral3, variance3, rms3)
+integral3, variance3, rms3, time3 = MonteCarlo_Integrator(function3,[-2,2],10000)
+print(integral3, variance3, rms3, time3)
 
-integral4, variance4, rms4 = MonteCarlo_Integrator(function4, [0,1,0,1], 10000)
-print(integral4, variance4, rms4)
+integral4, variance4, rms4, time4 = MonteCarlo_Integrator(function4, [0,1,0,1], 10000)
+print(integral4, variance4, rms4, time4)
 
 # Use your subroutine to evaluate the size of the region enclosed
 # within an n-sphere of radius 2.0, for n = 3 (i.e. the volume of a ball of
@@ -87,8 +90,8 @@ def sphere_func_5(x,y,z,rho,epsilon):
     else:
         return 0
 
-integral5, variance5, rms5 = MonteCarlo_Integrator(sphere_func_3, [-2,2,-2,2,-2,2], 10000)
-integral6, variance6, rms6 = MonteCarlo_Integrator(sphere_func_5, [-2,2,-2,2,-2,2,-2,2,-2,2], 10000)
-print(integral5, variance5, rms5)
-print(integral6, variance6, rms6)
+integral5, variance5, rms5, time5 = MonteCarlo_Integrator(sphere_func_3, [-2,2,-2,2,-2,2], 10000)
+integral6, variance6, rms6, time6 = MonteCarlo_Integrator(sphere_func_5, [-2,2,-2,2,-2,2,-2,2,-2,2], 10000)
+print(integral5, variance5, rms5, time5)
+print(integral6, variance6, rms6, time6)
 
